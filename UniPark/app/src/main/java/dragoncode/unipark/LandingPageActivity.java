62aabb755e109106693c1be18d72961308d6cf2f
Unknown <k.esterhuizen.ke@gmail.com>
2018-04-18 13:27:05 +0200

    Created navigation drawer
    Created navigation drawer
    Impliment navigation drawer to landing page
    Impliment a drawer layout to landing page
    Recreated all buttons on Drawer layout

--- UniPark/app/src/main/java/dragoncode/unipark/LandingPageActivity.java
+++ UniPark/app/src/main/java/dragoncode/unipark/LandingPageActivity.java
@@ -2,14 +2,39 @@ package dragoncode.unipark;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.MenuItem;
 
 public class LandingPageActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.landing_page);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.Open, R.string.Close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
