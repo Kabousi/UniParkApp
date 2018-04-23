21a674e1c9192b7e87517047a7005dcc07f9013b
Unknown <k.esterhuizen.ke@gmail.com>
2018-04-23 14:20:32 +0200

    Programmed buttons activity buttons
    Programmed buttons to go to respective activity

--- UniPark/app/src/main/java/dragoncode/unipark/LandingPageActivity.java
+++ UniPark/app/src/main/java/dragoncode/unipark/LandingPageActivity.java
@@ -2,6 +2,7 @@ package dragoncode.unipark;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,11 +10,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class LandingPageActivity extends AppCompatActivity {
+public class LandingPageActivity extends AppCompatActivity implements View.OnClickListener {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private Button btnViewProfile;
+    private Button btnViewParking;
+    private Button btnReportUser;
+    private Button btnMaps;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,17 @@ public class LandingPageActivity extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        btnViewProfile = (Button) findViewById(R.id.btn_profile);
+        btnViewParking = (Button) findViewById(R.id.btn_parking);
+        btnReportUser = (Button) findViewById(R.id.btn_report);
+        btnMaps = (Button) findViewById(R.id.btn_maps);
+
+        btnViewProfile.setOnClickListener(this);
+        btnMaps.setOnClickListener(this);
+        btnReportUser.setOnClickListener(this);
+        btnViewParking.setOnClickListener(this);
     }
 
     @Override
@@ -36,5 +55,32 @@ public class LandingPageActivity extends AppCompatActivity {
             return true;
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_profile:
+                intent = new Intent(this, ViewProfileActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btn_parking:
+                intent = new Intent(this, view_parking.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btn_report:
+                intent = new Intent(this, ViewProfileActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btn_maps:
+                intent = new Intent(this, ViewProfileActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
--- UniPark/app/src/main/java/dragoncode/unipark/Login.java
+++ UniPark/app/src/main/java/dragoncode/unipark/Login.java
@@ -1,14 +1,33 @@
 package dragoncode.unipark;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends Activity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
     }
+
+    private void openActivity(){
+        Intent intent = new Intent(this, LandingPageActivity.class);
+        startActivity(intent);
+    }
+
 }
