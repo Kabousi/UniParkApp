879160a0ae51dcdf2c6fbcf754665cbd216b7611
Unknown <k.esterhuizen.ke@gmail.com>
2018-05-09 15:20:03 +0200

    Created spinner for Request parking
    Added two java classes and an adapter for the spinner to inflate the spinner with values

---
+++ UniPark/app/src/main/java/dragoncode/unipark/parking_name_item.java
@@ -0,0 +1,13 @@
+package dragoncode.unipark;
+
+public class parking_name_item {
+    private String mparkingName;
+
+    public parking_name_item(String parkingName){
+        mparkingName = parkingName;
+    }
+
+    public String getparkingNameItem(){
+        return mparkingName;
+    }
+}
